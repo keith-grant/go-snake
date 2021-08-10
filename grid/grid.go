f1d69d93e68506c9aa855518385af0b29f582c5d
Keith Grant <keith.grant@eu.medical.canon>
2021-08-10 14:09:30 +0100

    Trying to only move into largest area

--- grid/grid.go
+++ grid/grid.go
@@ -1,158 +1,156 @@
 package grid
 
-
 import (
-  bs "github.com/BattlesnakeOfficial/starter-snake-go/gameTypes"
+	bs "github.com/BattlesnakeOfficial/starter-snake-go/gameTypes"
 )
+
 type CellType int32
 
 const (
-        EMPTY CellType = 0
-        FOOD  CellType = 1
-        SNAKE CellType = 2
+	EMPTY CellType = 0
+	FOOD  CellType = 1
+	SNAKE CellType = 2
 )
 
 /////////////////////
 type Cell struct {
-  position bs.Coord
-  Up* Cell
-  Right* Cell
-  Down* Cell
-  Left* Cell
-  Type CellType
-  DistanceToFood int
-  ConnectedCellCount int
+	position           bs.Coord
+	Up                 *Cell
+	Right              *Cell
+	Down               *Cell
+	Left               *Cell
+	Type               CellType
+	DistanceToFood     int
+	ConnectedCellCount int
 }
 
-
 func NewCell(x int, y int, cellType CellType) *Cell {
-    newCell := new(Cell)
-    newCell.position.X = x
-    newCell.position.Y = y
-    newCell.Up = nil
-    newCell.Right = nil
-    newCell.Down = nil
-    newCell.Left = nil
-    newCell.Type = cellType
-    return newCell
+	newCell := new(Cell)
+	newCell.position.X = x
+	newCell.position.Y = y
+	newCell.Up = nil
+	newCell.Right = nil
+	newCell.Down = nil
+	newCell.Left = nil
+	newCell.Type = cellType
+	return newCell
 }
 
 func getCellType(state bs.GameState, x int, y int) CellType {
-  retVal := EMPTY
-  if(isSnake(state, x, y)){
-    retVal = SNAKE
-  } else if(isFood(state, x, y)){
-    retVal = FOOD
-  }
-  return retVal
+	retVal := EMPTY
+	if isSnake(state, x, y) {
+		retVal = SNAKE
+	} else if isFood(state, x, y) {
+		retVal = FOOD
+	}
+	return retVal
 }
 
-func isSnake(state bs.GameState, x int, y int) bool{
-  snakeList := state.Board.Snakes
-  for snakeindex := 0; snakeindex < len(snakeList); snakeindex++ {
-    snake := snakeList[snakeindex]
-    for segmentindex := 0; segmentindex < len(snake.Body); segmentindex ++ {
-      segment := snake.Body[segmentindex]
-      if(x == segment.X && y == segment.Y){
-        return true
-      }
-    }
-  }
-  return false
+func isSnake(state bs.GameState, x int, y int) bool {
+	snakeList := state.Board.Snakes
+	for snakeindex := 0; snakeindex < len(snakeList); snakeindex++ {
+		snake := snakeList[snakeindex]
+		for segmentindex := 0; segmentindex < len(snake.Body); segmentindex++ {
+			segment := snake.Body[segmentindex]
+			if x == segment.X && y == segment.Y {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func isFood(state bs.GameState, x int, y int) bool {
-  foodList := state.Board.Food
-  for foodindex := 0; foodindex < len(foodList); foodindex++ {
-    food := foodList[foodindex]
-    if(x == food.X && y == food.Y){
-      return true
-    }
-  }
-  return false
+	foodList := state.Board.Food
+	for foodindex := 0; foodindex < len(foodList); foodindex++ {
+		food := foodList[foodindex]
+		if x == food.X && y == food.Y {
+			return true
+		}
+	}
+	return false
 }
 
-func connectToSiblings(cell *Cell, grid* Grid){
-  cell.Up = GetCell(cell.position.X, cell.position.Y + 1, grid)
-  cell.Down = GetCell(cell.position.X, cell.position.Y - 1, grid)
-  cell.Left = GetCell(cell.position.X - 1, cell.position.Y, grid)
-  cell.Right = GetCell(cell.position.X + 1, cell.position.Y, grid)
+func connectToSiblings(cell *Cell, grid *Grid) {
+	cell.Up = GetCell(cell.position.X, cell.position.Y+1, grid)
+	cell.Down = GetCell(cell.position.X, cell.position.Y-1, grid)
+	cell.Left = GetCell(cell.position.X-1, cell.position.Y, grid)
+	cell.Right = GetCell(cell.position.X+1, cell.position.Y, grid)
 }
 
-func GetCell(x int, y int, grid* Grid) *Cell{
+func GetCell(x int, y int, grid *Grid) *Cell {
 
-  // check boundaries
-  if(x < 0){
-    return nil
-  }
-  if(y < 0) {
-    return nil
-  }
-  if (x > grid.width - 1) {
-    return nil
-  }
-  if (y > grid.height - 1) {
-    return nil
-  }
+	// check boundaries
+	if x < 0 {
+		return nil
+	}
+	if y < 0 {
+		return nil
+	}
+	if x > grid.width-1 {
+		return nil
+	}
+	if y > grid.height-1 {
+		return nil
+	}
 
-  return grid.allCells[bs.Coord{X:x, Y:y}]
+	return grid.allCells[bs.Coord{X: x, Y: y}]
 }
 
 //////////////////////////////////
 
 type Grid struct {
-  width int
-  height int
-  allCells map[bs.Coord]*Cell
+	width    int
+	height   int
+	allCells map[bs.Coord]*Cell
 }
 
-func CreateGrid(state bs.GameState) *Grid{
-  grid := new(Grid)
-  grid.width = state.Board.Width
-  grid.height = state.Board.Height
-  grid.allCells = make(map[bs.Coord]*Cell)
+func CreateGrid(state bs.GameState) *Grid {
+	grid := new(Grid)
+	grid.width = state.Board.Width
+	grid.height = state.Board.Height
+	grid.allCells = make(map[bs.Coord]*Cell)
 
-  // Populate all cells
-  for x := 0; x < state.Board.Width;  x++ {
-    for y := 0; y < state.Board.Height;  y++ {
-      cellType := getCellType(state, x, y)
-      grid.allCells[bs.Coord{X: x, Y:y}] = NewCell(x, y, cellType)
-    }
-  }
+	// Populate all cells
+	for x := 0; x < state.Board.Width; x++ {
+		for y := 0; y < state.Board.Height; y++ {
+			cellType := getCellType(state, x, y)
+			grid.allCells[bs.Coord{X: x, Y: y}] = NewCell(x, y, cellType)
+		}
+	}
 
-  // connect all cells
-  for _, element := range grid.allCells{
-    connectToSiblings(element, grid)
-  }
+	// connect all cells
+	for _, element := range grid.allCells {
+		connectToSiblings(element, grid)
+	}
 
-  for _, element := range grid.allCells{
-    element.DistanceToFood = distanceToFood(*element, state)
-  }
+	for _, element := range grid.allCells {
+		element.DistanceToFood = distanceToFood(*element, state)
+	}
 
-  return grid
+	return grid
 }
 
-func GetCellPosition(cell *Cell) bs.Coord{
-  return cell.position
+func GetCellPosition(cell *Cell) bs.Coord {
+	return cell.position
 }
 
-func distanceToFood(cell Cell, state bs.GameState) int{
-  foodList := state.Board.Food
+func distanceToFood(cell Cell, state bs.GameState) int {
+	foodList := state.Board.Food
 
-  distanceToFood := state.Board.Width + state.Board.Height // max possible distance on a grid
-  for entry := range foodList{
-    distanceToEntry := distanceBetweenCells(cell.position, foodList[entry])
-    if distanceToEntry < distanceToFood {
-      distanceToFood = distanceToEntry
-    }
-  }
+	distanceToFood := state.Board.Width + state.Board.Height // max possible distance on a grid
+	for entry := range foodList {
+		distanceToEntry := distanceBetweenCells(cell.position, foodList[entry])
+		if distanceToEntry < distanceToFood {
+			distanceToFood = distanceToEntry
+		}
+	}
 
-  return distanceToFood
+	return distanceToFood
 }
-
 
 func distanceBetweenCells(a bs.Coord, b bs.Coord) int {
-  return intAbs(a.X - b.X) + intAbs(a.Y - b.Y)
+	return intAbs(a.X-b.X) + intAbs(a.Y-b.Y)
 }
 
 func intAbs(x int) int { // No built in integer abs !!!!
@@ -162,74 +160,88 @@ func intAbs(x int) int { // No built in integer abs !!!!
 	return x
 }
 
-func isConnected(thisCell *Cell, targetCell *Cell) bool{
-  return false
+func isConnected(thisCell *Cell, targetCell *Cell) bool {
+	return false
 }
 
 type void struct{}
 
 func ConnectedCellCount(cell *Cell) int {
 
-   connectedCellMap := make(map[*Cell]struct{})
+	connectedCellMap := make(map[*Cell]struct{})
 
-  //early out
-  _, found := connectedCellMap[cell]
-  if(found) {
-    // don't look at this cell again
-    return 0
-  }
-  // For each attached cell add it to the connectedCellMap and then call this on it
-  attachedCells := getAttachedCells(cell)
-  for entry := range attachedCells{
-    var member void
-    connectedCellMap[attachedCells[entry]]  = member
-  }
+	//early out
+	_, found := connectedCellMap[cell]
+	if found {
+		// don't look at this cell again
+		return 0
+	}
+	// For each attached cell add it to the connectedCellMap and then call this on it
+	attachedCells := getAttachedCells(cell)
+	for entry := range attachedCells {
+		var member void
+		connectedCellMap[attachedCells[entry]] = member
+	}
 
-  // then call connectedCells on all attachedCells
-  for entry := range attachedCells{
-    connectedCellCountInternal(attachedCells[entry], connectedCellMap)
-  }
-  return len(connectedCellMap)
+	// then call connectedCells on all attachedCells
+	for entry := range attachedCells {
+		connectedCellCountInternal(attachedCells[entry], connectedCellMap)
+	}
+	return len(connectedCellMap)
 }
 
-func connectedCellCountInternal(cell *Cell, connectedCellMap map[*Cell]struct{}) int {
+func connectedCellCountInternal(cell *Cell, connectedCellMap map[*Cell]struct{}) {
 
-  //early out
-  _, found := connectedCellMap[cell]
-  if(found) {
-    // don't look at this cell again
-    return 0
-  }
-  // For each attached cell add it to the connectedCellMap and then call this on it
-  attachedCells := getAttachedCells(cell)
-  for entry := range attachedCells{
-    var member void
-    connectedCellMap[attachedCells[entry]]  = member
-  }
+	// For each attached cell add it to the connectedCellMap and then call this on it
+	attachedCells := getAttachedCells(cell)
 
-  // then call connectedCells on all attachedCells
-  for entry := range attachedCells{
-    connectedCellCountInternal(attachedCells[entry], connectedCellMap)
-  }
-  return len(connectedCellMap)
+	// Keep a note of which attached Cells are being seen for teh first time
+	newCells := make(map[*Cell]struct{})
+	for entry := range attachedCells {
+		_, exists := connectedCellMap[attachedCells[entry]]
+		if !exists {
+			var member void
+			newCells[attachedCells[entry]] = member
+		}
+	}
+
+	// Add All attached cells to teh set of connected cells
+	for entry := range attachedCells {
+		var member void
+		connectedCellMap[attachedCells[entry]] = member
+	}
+
+	// then call connectedCells only on newCells
+	for key := range newCells {
+		connectedCellCountInternal(key, connectedCellMap)
+	}
 }
-
 
 func getAttachedCells(cell *Cell) []*Cell {
-  var attachedCells []*Cell
+	var attachedCells []*Cell
 
-  if(cell.Up != nil){
-    attachedCells = append(attachedCells, cell.Up)
-  }
-  if(cell.Right != nil){
-    attachedCells = append(attachedCells, cell.Right)
-  }
-  if(cell.Down != nil){
-    attachedCells = append(attachedCells, cell.Down)
-  }
-  if(cell.Left != nil){
-    attachedCells = append(attachedCells, cell.Left)
-  }
+	if validCell(cell.Up) {
+		attachedCells = append(attachedCells, cell.Up)
+	}
+	if validCell(cell.Right) {
+		attachedCells = append(attachedCells, cell.Right)
+	}
+	if validCell(cell.Down) {
+		attachedCells = append(attachedCells, cell.Down)
+	}
+	if validCell(cell.Left) {
+		attachedCells = append(attachedCells, cell.Left)
+	}
 
-  return attachedCells
+	return attachedCells
+}
+
+func validCell(cell *Cell) bool {
+	if cell == nil {
+		return false
+	}
+	if cell.Type == SNAKE {
+		return false
+	}
+	return true
 }
--- logic.go
+++ logic.go
@@ -7,9 +7,9 @@ package main
 
 import (
 	"log"
-  //"math"
-  gb "github.com/BattlesnakeOfficial/starter-snake-go/grid"
-  bs "github.com/BattlesnakeOfficial/starter-snake-go/gameTypes"
+	//"math"
+	bs "github.com/BattlesnakeOfficial/starter-snake-go/gameTypes"
+	gb "github.com/BattlesnakeOfficial/starter-snake-go/grid"
 )
 
 // This function is called when you register your Battlesnake on play.battlesnake.com
@@ -35,7 +35,6 @@ func start(state bs.GameState) {
 	log.Printf("%s START\n", state.Game.ID)
 }
 
-
 // This function is called when a game your Battlesnake was in has ended.
 // It's purely for informational purposes, you don't have to make any decisions here.
 func end(state bs.GameState) {
@@ -47,12 +46,12 @@ func end(state bs.GameState) {
 // We've provided some code and comments to get you started.
 func move(state bs.GameState) BattlesnakeMoveResponse {
 
-  boardGrid := gb.CreateGrid(state)
+	boardGrid := gb.CreateGrid(state)
 
-  up := bs.Coord{X: state.You.Head.X, Y: state.You.Head.Y + 1}
-  down := bs.Coord{X: state.You.Head.X, Y: state.You.Head.Y - 1}
-  left := bs.Coord{X: state.You.Head.X -1, Y: state.You.Head.Y}
-  right := bs.Coord{X: state.You.Head.X + 1, Y: state.You.Head.Y}
+	up := bs.Coord{X: state.You.Head.X, Y: state.You.Head.Y + 1}
+	down := bs.Coord{X: state.You.Head.X, Y: state.You.Head.Y - 1}
+	left := bs.Coord{X: state.You.Head.X - 1, Y: state.You.Head.Y}
+	right := bs.Coord{X: state.You.Head.X + 1, Y: state.You.Head.Y}
 
 	possibleMoves := map[string]*gb.Cell{
 		"up":    gb.GetCell(up.X, up.Y, boardGrid),
@@ -60,20 +59,20 @@ func move(state bs.GameState) BattlesnakeMoveResponse {
 		"left":  gb.GetCell(left.X, left.Y, boardGrid),
 		"right": gb.GetCell(right.X, right.Y, boardGrid),
 	}
- 
-  //log.Print(boardGrid)
+
+	//log.Print(boardGrid)
 	// TODO: Step 1 - Don't hit walls.
 	// Use information in GameState to prevent your Battlesnake from moving beyond the boundaries of the board.
 	// boardWidth := state.Board.Width
 	// boardHeight := state.Board.Height
 
-//  avoidWalls(state, possibleMoves)
+	//  avoidWalls(state, possibleMoves)
 
 	// TODO: Step 2 - Don't hit yourself.
 	// Use information in GameState to prevent your Battlesnake from colliding with itself.
 	// mybody := state.You.Body
 
-//  avoidSelf(state, possibleMoves)
+	//  avoidSelf(state, possibleMoves)
 
 	// TODO: Step 3 - Don't collide with others.
 	// Use information in GameState to prevent your Battlesnake from colliding with others.
@@ -87,33 +86,32 @@ func move(state bs.GameState) BattlesnakeMoveResponse {
 
 	safeMoves := make(map[string]*gb.Cell)
 	for move, cell := range possibleMoves {
-    if cell != nil{
-      //log.Printf("%s : %+v\n", move, cell)
-    } else {
-      //log.Printf("%s : is nil\n", move)
-    }
+		if cell != nil {
+			//log.Printf("%s : %+v\n", move, cell)
+		} else {
+			//log.Printf("%s : is nil\n", move)
+		}
 		if isSafe(cell, state.You.Body) {
 			safeMoves[move] = cell
 		}
 	}
 
+	safeMovesInfo := make(map[string]int)
 
-  safeMovesInfo := make(map[string]int)
+	for key, entry := range safeMoves {
+		entry.ConnectedCellCount = gb.ConnectedCellCount(entry)
+		safeMovesInfo[key] = entry.ConnectedCellCount
+	}
 
-  for key, entry := range safeMoves{
-    entry.ConnectedCellCount = gb.ConnectedCellCount(entry);
-    safeMovesInfo[key] = entry.ConnectedCellCount
-  }
-
-  log.Print(safeMovesInfo)
+	log.Print(safeMovesInfo)
 	if len(safeMoves) == 0 {
 		nextMove = "down"
-    //prefer the wall to eating yourself but if there are no walls then default to down
-    for move, cell := range possibleMoves {
-      if cell == nil {
-        nextMove = move
-      }
-    }
+		//prefer the wall to eating yourself but if there are no walls then default to down
+		for move, cell := range possibleMoves {
+			if cell == nil {
+				nextMove = move
+			}
+		}
 		log.Printf("%s MOVE %d: No safe moves detected! Moving %s\n", state.Game.ID, state.Turn, nextMove)
 	} else {
 		nextMove = decideMove(safeMoves, state)
@@ -124,58 +122,73 @@ func move(state bs.GameState) BattlesnakeMoveResponse {
 	}
 }
 
-func isSafe(cell* gb.Cell, me []bs.Coord) bool{
-  if cell == nil {
-    return false
-  }
-  switch cell.Type{
-    case gb.EMPTY:
-    return true
-    case gb.FOOD:
-    return true
-    case gb.SNAKE:
-    return false
-  }
+func isSafe(cell *gb.Cell, me []bs.Coord) bool {
+	if cell == nil {
+		return false
+	}
+	switch cell.Type {
+	case gb.EMPTY:
+		return true
+	case gb.FOOD:
+		return true
+	case gb.SNAKE:
+		return false
+	}
 
-  return isMe(gb.GetCellPosition(cell).X, gb.GetCellPosition(cell).Y, me)
+	return isMe(gb.GetCellPosition(cell).X, gb.GetCellPosition(cell).Y, me)
 }
 
 func isMe(targetX int, targetY int, me []bs.Coord) bool {
-  for i := 0; i < len(me);  i++ {
-    if(me[i].X == targetX && me[i].Y == targetY) {
-      return true
-    }
-  }
-  return false
+	for i := 0; i < len(me); i++ {
+		if me[i].X == targetX && me[i].Y == targetY {
+			return true
+		}
+	}
+	return false
 }
 
-func decideMove(options map[string]*gb.Cell, state bs.GameState) string{
-  starting_value := 500
-  distanceToFood := starting_value
-  var move string
+func decideMove(options map[string]*gb.Cell, state bs.GameState) string {
+	starting_value := 5000
+	distanceToFood := starting_value
+	var move string
 
-  imHungry := hungry(state.You)
-  for key, entry := range options{
-    // if hungary move towards FOOD
-    if(imHungry) {
-      if(entry.DistanceToFood < distanceToFood) {
-        move = key
-        distanceToFood = entry.DistanceToFood
-      }
-    } else {
-      // move away from food
-      if(entry.DistanceToFood > distanceToFood || distanceToFood == starting_value) {
-        move = key
-        distanceToFood = entry.DistanceToFood
-      }
-    }
-  }
-  return move
+	largestSpace := -1
+	// find largest space to move into
+	for _, entry := range options {
+		if entry.ConnectedCellCount > largestSpace {
+			largestSpace = entry.ConnectedCellCount
+		}
+	}
+
+	// Now remove all cells that aren't connected to a space that large
+	for key, entry := range options {
+		if entry.ConnectedCellCount != largestSpace {
+			delete(options, key)
+		}
+	}
+
+	iAmHungry := hungry(state.You)
+	for key, entry := range options {
+		// if hungary move towards FOOD
+		if iAmHungry {
+			if entry.DistanceToFood < distanceToFood {
+				move = key
+				distanceToFood = entry.DistanceToFood
+			}
+		} else {
+			// move away from food
+			if entry.DistanceToFood > distanceToFood || distanceToFood == starting_value {
+				move = key
+				distanceToFood = entry.DistanceToFood
+			}
+		}
+	}
+	return move
 }
 
 func hungry(you bs.Battlesnake) bool {
-  if you.Health < 20 {
-    return true
-  }
-  return false
+	if you.Health < 20 {
+		return true
+	}
+	return false
 }
